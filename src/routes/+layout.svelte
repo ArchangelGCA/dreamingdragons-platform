21a1d9a48cea7941055a39ce746781a1559b659a
AnonymousGCA <archangelgca@gmail.com>
2024-02-05 19:00:26 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+
+    import {onMount} from "svelte";
+
+    onMount(() => {
+        // Close navbar when open another page, with animation
+        document.querySelectorAll('.nav-link').forEach((element) => {
+            element.addEventListener('click', () => {
+                document.querySelector('.navbar-collapse').classList.remove('show');
+            });
+        });
+
+        window.$('[data-bs-toggle="tooltip"]').tooltip();
+
+        // Init AOS
+        AOS.init({
+            once: true,
+        });
+        window.AOS = AOS;
+    });
+
+    // Automatically update footer year
+    const currentYear = new Date().getFullYear(); // Will use this in the footer to automatically update the year
+</script>
+
+<!-- TODO: Navbar -->
+
+<slot></slot>
+
+<!-- TODO: Footer -->
+
+<style>
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {onMount} from "svelte";
+
+    onMount(() => {
+        window.$('[data-bs-toggle="tooltip"]').tooltip();
+    });
+</script>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center">If you can see this, the project is working!</h1>
+        </div>
+    </div>
+</div>
