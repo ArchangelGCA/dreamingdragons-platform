7885d598d0a374251c5fdc0c8ae8c2fae7dcd7f6
AnonymousGCA <archangelgca@gmail.com>
2024-02-11 03:07:33 +0100

    Changelogs: - Testing adapter-vercel. - Testing images uploads and conversion on the edge. - Minor changes.
    TODO:
    - Try installing sharp and check if it works on Vercel.
    - Redesign of upload page.
    - Complete client and server for chapter submission.
    - Make TOS and Privacy Policy pages.
    - Everything else.

--- src/routes/upload/+page.server.js
+++ src/routes/upload/+page.server.js
@@ -43,6 +43,10 @@ const uploadImage = async (image, s3) => {
     return `${STORJ_SHARE_LINK}/${STORJ_BUCKET_NAME}/${coverUrl}?wrap=0`;
 }
 
+export const config = {
+    runtime: 'edge',
+};
+
 export const load = async ({ locals: { supabase, getSession/*, s3*/ } }) => {
     const session = await getSession();
 
