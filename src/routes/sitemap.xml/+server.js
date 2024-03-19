5d76f6926e011ba00de545b84e5f99b8451a374e
ArchangelGCA <archangelgca@gmail.com>
2024-03-19 18:55:02 +0100

    Changelogs: - Added profiles to sitemap.xml. - Minor changes.
    TODO:
    - Complete comments section for both Books and Chapters (backend, server, and client)
    - Add range pagination for API.
    - Search page /search.
    - Comments counter, with actual comments (TDB).
    - System to manage follows.
    - Redesign of upload page.
    - Make TOS and Privacy Policy pages.
    - Everything else.

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -2,16 +2,19 @@ import * as sitemap from 'super-sitemap';
 
 export const GET = async ({locals: {supabase, getSession}}) => {
 
-    // get from books table of supabase and return the list of books
-    const { data: books, error } = await supabase
-        .from('book')
-        .select('id');
+    const [books, chapters, profiles] = await Promise.all([
+        supabase.from('book').select('id'),
+        supabase.from('chapters').select('id, book_id'),
+        supabase.from('profiles').select('id')
+    ]);
 
-    const {data: chapters, error: error2} = await supabase
-        .from('chapters')
-        .select('id, book_id');
+    // Access data and error from each response
+    const { data: booksData, error: booksError } = books;
+    const { data: chaptersData, error: chaptersError } = chapters;
+    const { data: profilesData, error: profilesError } = profiles;
 
-    if (error || error2) {
+    // Handle errors if any
+    if (booksError || chaptersError || profilesError) {
         // empty books array
         return await sitemap.response({
             origin: 'https://tales.rosesintheflames.com',
@@ -19,14 +22,18 @@ export const GET = async ({locals: {supabase, getSession}}) => {
                 '/content/[book]': [],
                 '/content/[book]/[chapter]': [],
             },
+            additionalPaths: []
         });
     }
 
     return await sitemap.response({
         origin: 'https://tales.rosesintheflames.com',
         paramValues: {
-            '/content/[book]': books.map((book) => book.id),
-            '/content/[book]/[chapter]': chapters.map((chapter) => [chapter.book_id, chapter.id]),
+            '/content/[book]': booksData.map((book) => book.id),
+            '/content/[book]/[chapter]': chaptersData.map((chapter) => [chapter.book_id, chapter.id]),
         },
+        additionalPaths: [
+            ...(profilesData.map((profile) => `/profile?id=${profile.id}`))
+        ]
     });
 };
