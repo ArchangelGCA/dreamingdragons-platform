2bc5f64860aed24ba4898f7cb35e0dd2065ec95f
ArchangelGCA <archangelgca@gmail.com>
2024-04-09 20:28:42 +0200

    Changelogs: - Added to /settings/updates mailing list some metadata for twitter and cards. - Minor changes.
    TODO:
    - Add to avatar icon to top navbar dropdown menu.
    - System to reorder Chapters.
    - Add range pagination for API on multiple pages, such as /profile.
    - Complete homepage, with pagination too.
    - Everything else.

--- src/routes/settings/updates/+page.svelte
+++ src/routes/settings/updates/+page.svelte
@@ -35,6 +35,18 @@
         activeEvent = false;
     }
 </script>
+<svelte:head>
+    <title>Roses In The Flames | Home</title>
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Roses In The Flames | Subscribe Mailing List" />
+    <meta name="twitter:description" content="Subscribe to the official Roses In The Flames mailing list." />
+    <meta name="twitter:image" content="https://tales.rosesintheflames.com/favicon.webp" />
+
+    <meta property="og:title" content="Roses In The Flames | Subscribe Mailing List" />
+    <meta property="og:image" content="https://tales.rosesintheflames.com/favicon.webp" />
+    <meta property="og:description" content="Subscribe to the official Roses In The Flames mailing list." />
+</svelte:head>
 
 <div class="container text-center">
     <div class="row d-flex align-items-center" style="min-height: 69.5vh">
