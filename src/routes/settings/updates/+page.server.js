d8141c17ec6734d53aa9eb8e506b35955924ac29
ArchangelGCA <archangelgca@gmail.com>
2024-04-10 16:55:22 +0200

    Changelogs: - Subscribing to mailing list will now email user, that also contains the code to unsubscribe. - Unsubscribe page now also works with user ID, in the future EMAIL parameters unsubscribe will be deprecated and removed. - Minor changes.
    TODO:
    - Add to avatar icon to top navbar dropdown menu.
    - System to reorder Chapters.
    - Add range pagination for API on multiple pages, such as /profile.
    - Complete homepage, with pagination too.
    - Everything else.

--- src/routes/settings/updates/+page.server.js
+++ src/routes/settings/updates/+page.server.js
@@ -69,13 +69,117 @@ export const actions = {
         // extract content before @ in the email and use it as firstname and lastname
         const nameAndLastname = email.split('@')[0];
 
-        await resend.contacts.create({
+        const result = await resend.contacts.create({
             email: email,
             firstname: nameAndLastname,
             lastname: nameAndLastname,
             unsubscribed: false,
             audienceId: PRIVATE_RESEND_AUDIENCE_ID
         });
+
+        if (!result || result.data === null || result.data.id === null) {
+            return {
+                status: 400,
+                body: {
+                    message: 'Failed to subscribe to mailing list!'
+                }
+            }
+        }
+
+        const unsubscribeLink = `https://tales.rosesintheflames.com/settings/updates/unsubscribe?id=${result.data.id}`;
+        const discordLink = 'https://discord.gg/rosesintheflames';
+        const yearCopyright = new Date().getFullYear();
+
+        const htmlEmail = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 0;
+                        padding: 0;
+                        background-color: #f4f4f4;
+                    }
+            
+                    .container {
+                        width: 80%;
+                        margin: auto;
+                        overflow: hidden;
+                    }
+            
+                    .header {
+                        background: #33a8a5;
+                        color: white;
+                        padding: 10px 0;
+                        text-align: center;
+                    }
+            
+                    .header h1 {
+                        margin: 0;
+                    }
+            
+                    .content {
+                        padding: 30px;
+                        background-color: white;
+                        color: #333;
+                        line-height: 1.6em;
+                    }
+            
+                    .button {
+                        display: inline-block;
+                        background-color: #33a8a5;
+                        color: white;
+                        padding: 10px 20px;
+                        text-decoration: none;
+                        border-radius: 5px;
+                        margin: 10px 0;
+                    }
+            
+                    .footer {
+                        background: #333;
+                        color: white;
+                        text-align: center;
+                        padding: 10px;
+                        margin-top: 20px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <div class="header">
+                        <h1>Welcome to RiTF Mailing List!</h1>
+                    </div>
+                    <div class="content">
+                        <h2>You've successfully signup to our mailing list!</h2>
+                        <p>If you wish to unsubscribe, please click the button or use the link above: </p>
+                        <a href="${unsubscribeLink}" class="button">Unsubscribe</a>
+                        <p><a href="${unsubscribeLink}">${unsubscribeLink}</a></p>
+                        <p>Please keep this link safe! it has your <b>unsubscribe code</b>!</p>
+                        <p>If you have any issues, please reach us on our <a href="${discordLink}">Discord Server!</a></p>
+                    </div>
+                    <div class="footer">
+                        <p>&copy; ${yearCopyright} RosesInTheFlames. All rights reserved.</p>
+                    </div>
+                </div>
+            </body>
+            </html>
+        `;
+
+        const sent = await resend.emails.send({
+            from: 'RiTF <mailing@tales.rosesintheflames.com>',
+            to: [email],
+            subject: 'Welcome to the mailing list! - Roses In The Flames',
+            html: htmlEmail,
+            tags: [
+                {
+                    name: 'category',
+                    value: 'updates'
+                },
+            ],
+        });
+
+        console.log('Email sent:', sent);
 
         return {
             status: 200,
--- src/routes/settings/updates/unsubscribe/+server.js
+++ src/routes/settings/updates/unsubscribe/+server.js
@@ -4,33 +4,49 @@ import { Resend } from 'resend';
 // Example usage: settings/updates/unsubscribe?email=myemailgca@gmail.com
 export const GET = async ({ url }) => {
     const email = url.searchParams.get('email');
+    const idFromUrl = url.searchParams.get('id');
 
-    if (!email || email === '') {
-        return new Response('Email is required!', { status: 400 });
+    if ((!email || email === '') && (!idFromUrl || idFromUrl === '')) {
+        return new Response('Email or id is required!', { status: 400 });
     }
 
     const resend = new Resend(PRIVATE_RESEND_API_KEY);
 
-    // Get contacts from audience
-    const audienceList = await resend.contacts.list({
-        audienceId: PRIVATE_RESEND_AUDIENCE_ID
-    });
-
     let id = null;
 
-    // Check if email is already registered
-    if (audienceList.data.data.length > 0) {
-        const isEmailRegistered = audienceList.data.data.find(contact => contact.email === email);
-        if (!isEmailRegistered || isEmailRegistered.unsubscribed) {
-            return new Response('Email is not subscribed or already unsubscribed!', { status: 400 });
-        } else {
-            id = isEmailRegistered.id;
-            /*if (isEmailRegistered.unsubscribed) {
-                return new Response('Email is already unsubscribed!', { status: 400 });
-            }*/
+    if (idFromUrl && idFromUrl !== '') {
+
+        id = idFromUrl;
+
+        const contact = await resend.contacts.get({
+            id: id,
+            audienceId: PRIVATE_RESEND_AUDIENCE_ID
+        });
+
+        if (!contact || contact.data === null) {
+            return new Response('Contact not found!', { status: 400 });
+        }
+
+        if (contact.data.unsubscribed) {
+            return new Response('Email is already unsubscribed!', { status: 400 });
+        }
+
+    } else {
+
+        const audienceList = await resend.contacts.list({
+            audienceId: PRIVATE_RESEND_AUDIENCE_ID
+        });
+
+        // Check if email is already registered
+        if (audienceList.data.data.length > 0) {
+            const isEmailRegistered = audienceList.data.data.find(contact => contact.email === email);
+            if (!isEmailRegistered || isEmailRegistered.unsubscribed) {
+                return new Response('Email is not subscribed or already unsubscribed!', {status: 400});
+            } else {
+                id = isEmailRegistered.id;
+            }
         }
     }
-
 
     await resend.contacts.update({
         id: id,
