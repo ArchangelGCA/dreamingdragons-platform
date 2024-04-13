b51e108453d6ae0fb19e705df31f691d0b1965f4
ArchangelGCA <archangelgca@gmail.com>
2024-04-13 12:10:06 +0200

    Changelogs: - Fixed sitemap.xml breaking with some weird tags. - Minor changes.
    TODO:
    - Add to avatar icon to top navbar dropdown menu.
    - System to reorder Chapters.
    - Add range pagination for API on multiple pages, such as /profile.
    - Complete homepage, with pagination too.
    - Everything else.

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -35,7 +35,7 @@ export const GET = async ({locals: {supabase, getSession}}) => {
             '/content/[book]/[chapter]': chaptersData.map((chapter) => [chapter.book_id, chapter.id]),
             '/profile/[profile]': profilesData.map((profile) => profile.id)
         },
-        additionalPaths: tagsData.map((tag) => `/search?q=${tag.name}`),
+        additionalPaths: tagsData.map((tag) => `/search?q=${encodeURIComponent(tag.name)}`),
         excludePatterns: [
             '^/edit.*'
         ]
