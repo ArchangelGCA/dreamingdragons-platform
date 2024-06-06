8032624078871b73bfd1538f5dbcce45fc7a3c37
ArchangelGCA <archangelgca@gmail.com>
2024-06-06 21:47:47 +0200

    Changelogs: - Updated homepage views fetching unnecessary data making first load a lot heavier. - Minor changes.
    REMINDER:
    - Try increasing images load step, it may help (decreases amount of refreshes for layout, which seem to be falling into a concurrent race).
    
    TODO:
    - System to reorder Chapters.
    - Add range pagination for API on multiple pages, such as /profile.
    - Complete homepage, with pagination too.
    - Everything else.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -30,14 +30,13 @@ export const load = async ( { locals: { supabase, getSession } }) => {
         return data;
     }
 
-
     let books_ordered_by_likes, books_ordered_by_created_at, books_ordered_by_latest_chapter;
 
     try {
         [books_ordered_by_likes, books_ordered_by_created_at, books_ordered_by_latest_chapter] = await Promise.all([
-            fetchBooks('secure_books_ordered_by_likes'),
+            fetchBooks('secure_books_ordered_by_likes_new'),
             fetchCreatedAtBooks(),
-            fetchBooks('secure_books_ordered_by_latest_chapter_created_at')
+            fetchBooks('secure_books_ordered_by_latest_chapter_created_at_new')
         ]);
     } catch (error) {
         console.error(error);
