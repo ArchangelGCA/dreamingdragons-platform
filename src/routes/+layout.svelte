cd7e6747d9a75d648cea9159c631c078a9c396f0
ArchangelGCA <archangelgca@gmail.com>
2024-06-06 13:29:47 +0200

    Changelogs: - Issue with views counter - Minor changes.
    REMINDER:
    - Try increasing images load step, it may help (decreases amount of refreshes for layout, which seem to be falling into a concurrent race).
    
    TODO:
    - System to reorder Chapters.
    - Add range pagination for API on multiple pages, such as /profile.
    - Complete homepage, with pagination too.
    - Everything else.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
     let searchTerm = '';
     let latestNotificationTimestamp = notifications.length > 0 ? notifications[0].created_at : null;
     const notifsUpdateInterval = 30000;
-    let maintenance = false;
+    let maintenance = true;
 
     if ($pageStore.url.searchParams.has('q')) {
         searchTerm = $pageStore.url.searchParams.get('q');
@@ -244,11 +244,11 @@
             <!-- Warning like row telling peoples that the website is in maintenance mode temporarily and there may be issues -->
             <div class="row border-top border-light-subtle pt-3 pb-2">
                 <div class="col">
-                    <div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
-                        <strong>We're back online!</strong>
+                    <div class="alert alert-warning alert-dismissible fade show mb-0" role="alert">
+                        <strong>Views Counter incorrect!</strong>
                         <!-- Little text with a few details about the maintenance -->
-                        <small class="text-muted d-block">We apologise for the unexpected downtime, however we're back online. Issues are still expected (please report them)! For any question please reach us on <a href="https://discord.gg/5d5kVrEBzS" target="_blank">Discord</a></small>
-                        <small class="text-muted d-block">Degraded performance started around: 31/05/2024 18:30AM UTC/GMT+2</small>
+                        <small class="text-muted d-block">The views counter for Content and Chapters seems to not count views correctly, we're working on a fix! For any question please reach us on <a href="https://discord.gg/5d5kVrEBzS" target="_blank">Discord</a></small>
+                        <small class="text-muted d-block">The issue started around: 05/05/2024 17:00AM UTC/GMT+2</small>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" on:click={() => {maintenance = false}}></button>
                     </div>
                 </div>
