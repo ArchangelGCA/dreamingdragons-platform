0ab1cb7e4b560b37f8ee1b219acd3572bbb5bc18
ArchangelGCA <archangelgca@gmail.com>
2024-07-20 01:05:46 +0200

    Changelogs: - Fixing incorrect Discord iframe at bottom of homepage (Thanks to user report). - Minor changes.
    TODO:
    - System to reorder Chapters.
    - Add range pagination for API on multiple pages, such as /profile.
    - Complete homepage, with pagination too.
    - Everything else.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -166,7 +166,7 @@
         </div>
         <div class="col-auto">
             <p class="h3 text-center"><a class="btn btn-purple btn-lg" href="https://discord.gg/8v5WucBuRe" target="_blank" use:tooltip={{...tooltipConfig}} title="Join Discord">RiTF</a></p>
-            <iframe class="rounded-3" src="https://discord.com/widget?id=1054013154822205450&theme=dark" title="Roses in The Flames Discord" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            <iframe class="rounded-3" src="https://discord.com/widget?id=1149939721980624986&theme=dark" title="Roses in The Flames Discord" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
     </div>
 </div>
