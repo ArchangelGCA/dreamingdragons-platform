ea035de2941d8919be44f44c15b6a8830e584da6
ArchangelGCA <archangelgca@gmail.com>
2024-10-18 18:37:27 +0200

    Changelogs: - Optimization changes to fonts and bootstrap CDN (moved to Cloudflare CDN). - Minor changes.
    TODO:
    - System to reorder Chapters.
    - Galleries.
    - All client-side supabase calls shall be moved to the server.
    - Everything else.
    
    KNOWN ISSUES:
    - Directly navigating between profiles doesn't reset scroller height to top for home and favourites.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
     let searchTerm = '';
     let latestNotificationTimestamp = notifications.length > 0 ? notifications[0].created_at : null;
     const notifsUpdateInterval = 30000;
-    let maintenance = false;
+    let maintenance = true;
 
     if ($pageStore.url.searchParams.has('q')) {
         searchTerm = $pageStore.url.searchParams.get('q');
@@ -265,10 +265,10 @@
             <div class="row border-top border-light-subtle pt-3 pb-2">
                 <div class="col">
                     <div class="alert alert-warning alert-dismissible fade show mb-0" role="alert">
-                        <strong>Views Counter incorrect!</strong>
+                        <strong>Image database maintenance!</strong>
                         <!-- Little text with a few details about the maintenance -->
                         <small class="text-muted d-block">Our storage-api is currently experiencing <b>degraded performance</b>. Uploads may fail and images may be unavailable! For any question please reach us on <a href="https://discord.gg/5d5kVrEBzS" target="_blank">Discord</a>.</small>
-                        <small class="text-muted d-block">The issue started on: 15/09/2024 23:30AM UTC/GMT+2</small>
+                        <small class="text-muted d-block">The issue started on: 18/10/2024 17:30AM UTC/GMT+2</small>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" on:click={() => {maintenance = false}}></button>
                     </div>
                 </div>
