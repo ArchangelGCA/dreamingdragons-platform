3fb63957a4e0d2b611e885788a5d3de7c8eb7795
ArchangelGCA <archangelgca@gmail.com>
2024-11-08 13:34:48 +0100

    Changelogs: - Removing some over-complex unnecessary features. - Minor changes.
    TODO:
    - Check for pages with old SEO implementation and rework them.
    - System to reorder Chapters.
    - Galleries.
    - All client-side supabase calls shall be moved to the server.
    - Everything else.
    
    KNOWN ISSUES:
    - Directly navigating between profiles doesn't reset scroller height to top for home and favourites.

--- src/lib/components/layout/ContentImage.svelte
+++ src/lib/components/layout/ContentImage.svelte
@@ -2,12 +2,16 @@
     export let url;
     export let alt;
 
-    $: isImageLoaded = false;
+    //$: isImageLoaded = false;
 </script>
 
+<img src="{url}" alt="{alt}" class="img-fluid rounded-4" style="max-height: 82vh">
+
+<!--
 <img src="{url}" alt="{alt}" class="{isImageLoaded ? '' : 'd-none'} img-fluid rounded-4" on:load={() => isImageLoaded = true} style="max-height: 82vh">
 {#if !isImageLoaded}
     <div class="placeholder-glow" style="height: 82vh">
         <div class="placeholder bg-light-subtle rounded-4 w-100 h-100"></div>
     </div>
 {/if}
+-->
