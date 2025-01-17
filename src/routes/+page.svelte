8fa342040cce2766bb9b5685e40b5ed2afb051a0
ArchangelGCA <archangelgca@gmail.com>
2025-01-17 11:37:23 +0100

    Changelogs: - Updated invite link to RiTF. - Minor changes.
    TODO:
    - Check for pages with old SEO implementation and rework them.
    - System to reorder Chapters.
    - Galleries.
    - All client-side supabase calls shall be moved to the server.
    - Everything else.
    
    KNOWN ISSUES:
    - Directly navigating between profiles doesn't reset scroller height to top for home and favourites.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -203,7 +203,7 @@
                     sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
         <div class="col-auto">
-            <p class="h3 text-center"><a class="btn btn-purple btn-lg" href="https://discord.gg/8v5WucBuRe"
+            <p class="h3 text-center"><a class="btn btn-purple btn-lg" href="https://discord.gg/7y7vXnD846"
                                          target="_blank" use:tooltip={{...tooltipConfig}} title="Join Discord">RiTF</a>
             </p>
             <iframe class="rounded-3" src="https://discord.com/widget?id=1149939721980624986&theme=dark"
