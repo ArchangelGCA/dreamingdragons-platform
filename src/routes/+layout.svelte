824fc09dab5e67f5529db5d89864502379d3de67
ArchangelGCA <archangelgca@gmail.com>
2025-01-27 22:13:55 +0100

    Changelogs: - Maintenance message updated.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -277,7 +277,7 @@
                     <div class="alert alert-warning alert-dismissible fade show mb-0" role="alert">
                         <strong>Ongoing migration!</strong>
                         <!-- Little text with a few details about the maintenance -->
-                        <small class="text-muted d-block">Following some <b>unfortunate events</b> (RiTF scammed the dev). We're migrating from tales.rosesintheflames.com to <a href="https://tales.archangelgca.eu">tales.archangelgca.eu</a> ! For any question please reach us on <a href="https://discord.gg/5d5kVrEBzS" target="_blank">Discord</a>.</small>
+                        <small class="text-muted d-block">Following some <b>unfortunate events</b> (RiTF scammed the dev). We're migrating from tales.rosesintheflames.com to <a href="https://tales.archangelgca.eu">tales.archangelgca.eu</a> ! Performance is degraded until testing is over! For any question please reach us on <a href="https://discord.gg/5d5kVrEBzS" target="_blank">Discord</a>.</small>
                         <small class="text-muted d-block">Issue started on: 23/01/2025 10:00AM UTC/GMT+2</small>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" on:click={() => {maintenance = false}}></button>
                     </div>
